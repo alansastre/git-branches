f26d531574d70347fa1acc9a1cfd60b1a09f9892
Alan Sastre <alan@alansastre.co>
2021-11-08 09:51:46 +0100

    Update Main.java

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,6 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola desde master");
+        System.out.println("Hola desde master desde github");
     }
 }
